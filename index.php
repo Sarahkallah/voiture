6caacdb0f157b72ab6d89be4d48227abf2668b2d
Sarahkallah <Sarah.kallah@student.isfsc.be>
2018-10-15 17:01:17 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Opengo</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="opengo.css"/>
+</head>
+<img src="image/img1.jpg" class="background" id=""/>
+<body>
+</body>
+</html>
